3bcfa1f0068a4cec494391273abdc3e632c07beb
abslan laverde <abslan.laverde@gmail.com>
2023-04-30 21:10:05 -0400

    code cleanup

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,4 @@
 function ExpenseItem() {
-  //Invalid JSX code
-  //return <div>Date</div><div><h2>Title</h2><div>Amount</div></div>;
-
-  //one way to resolve this is by surrounding the code with a root element.
   return (
     <div>
       <div>March 28th, 2021</div>
@@ -15,7 +11,3 @@ function ExpenseItem() {
 }
 
 export default ExpenseItem;
-
-//You must only have one root element per return statement, or JSX code block.
-//The code block : "<div>Date</div><div><h2>Title</h2><div>Amount</div></div>"" is not valid JSX code
-//because it has two root elements: <div>Date</div> and <div><h2>Title</h2><div>Amount</div></div>.
