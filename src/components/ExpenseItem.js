3dc5378c4cc0d334dfa4901161a6b7208fc554b0
abslan laverde <abslan.laverde@gmail.com>
2023-04-30 21:18:58 -0400

    adding styling to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
+//We have to tell React which files to include in the build process. We do this by importing them into our App.js file. We can import them by using the relative path
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>March 28th, 2021</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$249.67</div>
+        <div className='expense-item__price'>$249.67</div>
       </div>
     </div>
   );
