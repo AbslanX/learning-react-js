fbfb804d8e7df1b55340cb694f16131502bc9754
abslan laverde <abslan.laverde@gmail.com>
2023-04-30 21:54:28 -0400

    sharing data between components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+//this props object is passed to the component by React from the parent component
+//we can access the props object in the component by adding it as a parameter to the function
+//we can then access the properties of the props object by using dot notation
+function ExpenseItem(props) {
     const expenseDate = new Date(2021, 2, 28);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 249.67;
 
     return (
         <div className='expense-item'>
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className='expense-item__description'>
-            <h2>{expenseTitle}</h2>
-            <div className='expense-item__price'>${expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>${props.amount}</div>
         </div>
         </div>
     );
