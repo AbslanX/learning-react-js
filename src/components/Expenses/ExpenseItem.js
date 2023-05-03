9cda42cb3f055c8462e6ab362218f02e7eb0892e
abslan laverde <abslan.laverde@gmail.com>
2023-05-03 11:53:59 -0400

    code cleanup

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,12 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-//the useState function allows us to define values as state where changes to these values should reflect in the component function being called again
 import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  //useState is an example of a react hook
-  //react hooks all begin with use
-  //they must be used directly in the component function
-
-  //we have to assign an initial value to the state
-  //we will use props.title as the initial value
-  //the useState function returns an array with exactly two elements
-  //the first element is the current state value
-  //the second element is a function that allows us to update the state
+  
   const [title, setTitle] = useState(props.title);
 
-  // we pass the setTitle function to the onClick handler and pass to it the new value we want to assign to the state
   const clickHandler = () => {
     setTitle("Updated!");
     console.log(title);
