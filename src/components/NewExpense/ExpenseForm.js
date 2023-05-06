c9061d70614e485270aac6d6e4443dfa7a7063b9
abslan laverde <abslan.laverde@gmail.com>
2023-05-06 17:35:45 -0400

    This PR contains a lesson regarding passing data up to the app.js component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,67 +1,84 @@
-import React, { useState } from 'react';
-import './ExpenseForm.css';
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+//we are passing the function from the parent component to the child component
+//the function is passed as a prop
+//so now we add the props parameter to the function
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    //we are calling the function that we got from the parent component
+    //we are passing the data to the parent component
+    //instead of onSubmit, we can call onSaveExpenseData from the parent component
+    //and pass the expenseData to the parent component
     
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
 
-    const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-        
-
-    };
-
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-
-    };
-
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-
-    };
-   
-    const submitHandler = (event) => {
-        event.preventDefault();
-
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        };
-
-        console.log(expenseData);
-        setEnteredTitle('');
-        setEnteredAmount('');
-        setEnteredDate('');
-    };
-
-    return (
-
-        
-
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control"> 
-                    <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
-                </div>
-                <div className="new-expense__control"> 
-                    <label>Amount</label>
-                    <input type="number" min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler}/>
-                </div>
-                <div className="new-expense__control"> 
-                    <label>Date</label>
-                    <input type="date" min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler}/>
-                </div>
-            </div>
-            <div className="new-expense__actions">
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form>
-    )
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,28 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+  //creating a function to pass data from child to parent
+  //the value for the prop should be a function 
+  //onSaveExpenseData is the prop name and saveExpenseDataHandler is the function name
+  //by adding this prop to the child component, we can pass data from child to parent
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {  //enteredExpenseData is the data that we get from the child component
+    const expenseData = {
+      ...enteredExpenseData, //spread operator to add the new data to the existing data
+      id: Math.random().toString() //generating a random id
+    };
+
+    //using this same pattern, we are passing  the expense up to the App.js component
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
-
+ 
 export default NewExpense;
